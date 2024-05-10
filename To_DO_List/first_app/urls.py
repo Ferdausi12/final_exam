1e3409d861c0ab5f679f1777d4145aa879c75b92
Ferdausi12 <113506783+Ferdausi12@users.noreply.github.com>
2024-05-10 19:11:40 +0600

    Add files via upload
    complete

--- To_DO_List/first_app/urls.py
+++ To_DO_List/first_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('',views.home),
     path('add_task/',views.TaskAdd.as_view(),name='add_task'),
     path('show_task/',views.ShowTask.as_view(),name='show_task'),
+    path('update_task/<int:pk>',views.TaskUpdate.as_view(),name='update_task'),
+    path('delete_task/<int:pk>',views.DeleteTask.as_view(),name='delete_task'),
+    path('complete_task/',views.CompletedTask.as_view(), name='complete_task'),
 ]
--- To_DO_List/first_app/views.py
+++ To_DO_List/first_app/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from first_app.models import TaskModel
 from first_app.forms import TaskMoelForm
-from django.views.generic.edit import FormView,CreateView,UpdateView,DeleteView
-from django.views.generic import ListView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.views.generic import ListView,TemplateView
 from django.urls import reverse_lazy
-
+from django.urls import reverse
 # Create your views here.
 def home(request):
     return render(request,'base.html')
@@ -21,3 +21,29 @@ class ShowTask(ListView):
     model = TaskModel
     template_name = 'show_task.html'
     context_object_name = 'data'
+    
+
+    
+
+class TaskUpdate(UpdateView):
+    form_class = TaskMoelForm
+    model = TaskModel
+    template_name = 'edit_task.html'
+    success_url = reverse_lazy('show_task')
+    
+class DeleteTask(DeleteView):
+    model = TaskModel
+    template_name = 'delete_task.html'
+    success_url = reverse_lazy('show_task')
+    
+class CompletedTask(TemplateView):
+    template_name = 'completed_task.html'
+
+    def get_context_data(self, **kwargs):
+        task_id = self.request.GET.get('task_id')
+        if task_id:
+            task = TaskModel.objects.get(pk=task_id)
+            task.completed = True
+            task.save()
+        return super().get_context_data(**kwargs)
+    
